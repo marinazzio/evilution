ffbf7fdca985effdbc84ca7dd8761cd06c37b58b
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-12 23:03:42 +0700

    63: Ensure to restore mutated file (#67)
    * 63: Ensure to restore mutated file
    
    * Fix restore_original_source method to prevent unnecessary file read

--- lib/evilution/isolation/fork.rb
+++ lib/evilution/isolation/fork.rb
@@ -23,9 +23,18 @@ module Evilution
       ensure
         read_io&.close
         write_io&.close
+        restore_original_source(mutation)
       end
 
       private
+
+      def restore_original_source(mutation)
+        return if File.read(mutation.file_path) == mutation.original_source
+
+        File.write(mutation.file_path, mutation.original_source)
+      rescue StandardError => e
+        warn("Warning: failed to restore #{mutation.file_path}: #{e.message}")
+      end
 
       def execute_in_child(mutation, test_command)
         test_command.call(mutation)
