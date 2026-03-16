12b40ac4d0fb4baac986881c4bb7d907d2c2b0a4
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-16 22:16:52 +0700

    50: Refactor server_spec to use subject for server instance

--- spec/evilution/mcp/server_spec.rb
+++ spec/evilution/mcp/server_spec.rb
@@ -3,23 +3,20 @@
 require "evilution/mcp/server"
 
 RSpec.describe Evilution::MCP::Server do
+  subject(:server) { described_class.build }
+
   describe ".build" do
     it "returns an MCP::Server instance" do
-      server = described_class.build
-
       expect(server).to be_a(MCP::Server)
     end
 
     it "registers the mutate tool" do
-      server = described_class.build
       tools = server.instance_variable_get(:@tools)
 
       expect(tools.keys).to include("evilution-mutate")
     end
 
     it "sets server name and version" do
-      server = described_class.build
-
       expect(server.instance_variable_get(:@name)).to eq("evilution")
       expect(server.instance_variable_get(:@version)).to eq(Evilution::VERSION)
     end
