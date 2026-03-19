2ebda0b9e1e1de90cded35ab639416da59869e5d
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-19 20:12:23 +0700

    97: Implement RegexpMutation operator with corresponding specs (#163)

--- spec/evilution/mutator/registry_spec.rb
+++ spec/evilution/mutator/registry_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Evilution::Mutator::Registry do
   let(:registry) { described_class.new }
 
   describe ".default" do
-    it "returns a registry pre-loaded with all 23 operators" do
+    it "returns a registry pre-loaded with all 24 operators" do
       default_registry = described_class.default
 
       expect(default_registry).to be_a(described_class)
-      expect(default_registry.operator_count).to eq(23)
+      expect(default_registry.operator_count).to eq(24)
     end
 
     it "includes all expected operator classes" do
@@ -38,7 +38,8 @@ RSpec.describe Evilution::Mutator::Registry do
         Evilution::Mutator::Operator::ArgumentRemoval,
         Evilution::Mutator::Operator::BlockRemoval,
         Evilution::Mutator::Operator::ConditionalFlip,
-        Evilution::Mutator::Operator::RangeReplacement
+        Evilution::Mutator::Operator::RangeReplacement,
+        Evilution::Mutator::Operator::RegexpMutation
       ]
 
       expect(operators).to match_array(expected_operators)
