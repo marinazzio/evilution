2fec245b0ba5b33d78488214f3ee4256e794430e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-21 10:07:20 +0700

    171: Add always-matching regexp variant to RegexpMutation for enhanced mutation coverage

--- lib/evilution/mutator/operator/regexp_mutation.rb
+++ lib/evilution/mutator/operator/regexp_mutation.rb
@@ -5,14 +5,19 @@ module Evilution
     module Operator
       class RegexpMutation < Base
         NEVER_MATCH = 'a\A'
+        ALWAYS_MATCH = ".*"
+
+        REPLACEMENTS = [NEVER_MATCH, ALWAYS_MATCH].freeze
 
         def visit_regular_expression_node(node)
-          add_mutation(
-            offset: node.content_loc.start_offset,
-            length: node.content_loc.length,
-            replacement: NEVER_MATCH,
-            node: node
-          )
+          REPLACEMENTS.each do |replacement|
+            add_mutation(
+              offset: node.content_loc.start_offset,
+              length: node.content_loc.length,
+              replacement: replacement,
+              node: node
+            )
+          end
 
           super
         end
