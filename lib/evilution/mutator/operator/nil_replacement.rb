da8bc354ff71d9907cbf6ba03fc491fdef0fccb0
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-21 09:28:50 +0700

    168: Expand NilReplacement to include false, 0, and empty string variants for enhanced mutation coverage

--- lib/evilution/mutator/operator/nil_replacement.rb
+++ lib/evilution/mutator/operator/nil_replacement.rb
@@ -4,13 +4,17 @@ module Evilution
   module Mutator
     module Operator
       class NilReplacement < Base
+        REPLACEMENTS = %w[true false 0 ""].freeze
+
         def visit_nil_node(node)
-          add_mutation(
-            offset: node.location.start_offset,
-            length: node.location.length,
-            replacement: "true",
-            node: node
-          )
+          REPLACEMENTS.each do |replacement|
+            add_mutation(
+              offset: node.location.start_offset,
+              length: node.location.length,
+              replacement: replacement,
+              node: node
+            )
+          end
 
           super
         end
