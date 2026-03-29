1c4edf37aace621399e5cce54105bf389c8354f9
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-29 23:59:12 +0700

    434: Refactor apply method in SourceSurgeon to handle multi-byte UTF-8 characters and preserve original encoding

--- lib/evilution/ast/source_surgeon.rb
+++ lib/evilution/ast/source_surgeon.rb
@@ -4,8 +4,8 @@ require_relative "../ast"
 
 module Evilution::AST::SourceSurgeon
   def self.apply(source, offset:, length:, replacement:)
-    result = source.dup
-    result[offset, length] = replacement
-    result
+    binary = source.b
+    binary[offset, length] = replacement.b
+    binary.force_encoding(source.encoding)
   end
 end
