137c4af4c293ac480a2fc3b2721d834c656a2a9e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-30 11:32:49 +0700

    301: Implement pattern matching array mutations and add corresponding tests (#442)

--- spec/evilution/mutator/registry_spec.rb
+++ spec/evilution/mutator/registry_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Evilution::Mutator::Registry do
       default_registry = described_class.default
 
       expect(default_registry).to be_a(described_class)
-      expect(default_registry.operator_count).to eq(51)
+      expect(default_registry.operator_count).to eq(52)
     end
 
     it "includes all expected operator classes" do
@@ -66,7 +66,8 @@ RSpec.describe Evilution::Mutator::Registry do
         Evilution::Mutator::Operator::IndexToDig,
         Evilution::Mutator::Operator::IndexAssignmentRemoval,
         Evilution::Mutator::Operator::PatternMatchingGuard,
-        Evilution::Mutator::Operator::PatternMatchingAlternative
+        Evilution::Mutator::Operator::PatternMatchingAlternative,
+        Evilution::Mutator::Operator::PatternMatchingArray
       ]
 
       expect(operators).to match_array(expected_operators)
