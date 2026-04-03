e8e8ce9de8c8f26d0f05ec2dd08d8a08a9700c0a
Denis Kiselev <denis.kiselyov@gmail.com>
2026-04-03 12:48:27 +0700

    356: Implement defined? check mutations and update registry

--- spec/evilution/mutator/registry_spec.rb
+++ spec/evilution/mutator/registry_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Evilution::Mutator::Registry do
       default_registry = described_class.default
 
       expect(default_registry).to be_a(described_class)
-      expect(default_registry.operator_count).to eq(58)
+      expect(default_registry.operator_count).to eq(59)
     end
 
     it "includes all expected operator classes" do
@@ -73,7 +73,8 @@ RSpec.describe Evilution::Mutator::Registry do
         Evilution::Mutator::Operator::KeywordArgument,
         Evilution::Mutator::Operator::MultipleAssignment,
         Evilution::Mutator::Operator::YieldStatement,
-        Evilution::Mutator::Operator::SplatOperator
+        Evilution::Mutator::Operator::SplatOperator,
+        Evilution::Mutator::Operator::DefinedCheck
       ]
 
       expect(operators).to match_array(expected_operators)
