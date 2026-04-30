3bfa467b5016b43e40b464a1dbd17993954a1598
Denis Kiselev <denis.kiselyov@gmail.com>
2026-04-30 09:31:27 +0700

    838: Refactor process cleanup specs to remove redundant nonexistent_pid method and use fixed PIDs for error handling

--- spec/evilution/process_cleanup_spec.rb
+++ spec/evilution/process_cleanup_spec.rb
@@ -3,12 +3,6 @@
 require "evilution/process_cleanup"
 
 RSpec.describe Evilution::ProcessCleanup do
-  def nonexistent_pid
-    pid = Process.fork { exit!(0) }
-    Process.wait(pid)
-    pid
-  end
-
   describe ".safe_kill" do
     it "delegates to Process.kill" do
       pid = Process.fork { sleep 5 }
@@ -19,11 +13,15 @@ RSpec.describe Evilution::ProcessCleanup do
     end
 
     it "swallows Errno::ESRCH for an unknown pid" do
-      expect { described_class.safe_kill("TERM", nonexistent_pid) }.not_to raise_error
+      allow(Process).to receive(:kill).and_raise(Errno::ESRCH)
+
+      expect { described_class.safe_kill("TERM", 999_999) }.not_to raise_error
     end
 
     it "returns nil when the process does not exist" do
-      expect(described_class.safe_kill("TERM", nonexistent_pid)).to be_nil
+      allow(Process).to receive(:kill).and_raise(Errno::ESRCH)
+
+      expect(described_class.safe_kill("TERM", 999_999)).to be_nil
     end
   end
 
@@ -35,11 +33,15 @@ RSpec.describe Evilution::ProcessCleanup do
     end
 
     it "swallows Errno::ECHILD when no child to reap" do
-      expect { described_class.safe_wait(nonexistent_pid) }.not_to raise_error
+      allow(Process).to receive(:wait).and_raise(Errno::ECHILD)
+
+      expect { described_class.safe_wait(999_999) }.not_to raise_error
     end
 
     it "returns nil when there is no child" do
-      expect(described_class.safe_wait(nonexistent_pid)).to be_nil
+      allow(Process).to receive(:wait).and_raise(Errno::ECHILD)
+
+      expect(described_class.safe_wait(999_999)).to be_nil
     end
   end
 end
