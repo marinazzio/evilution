f9a235dc24f295b32b0eab0aa9fe1225c2c08534
Denis Kiselev <denis.kiselyov@gmail.com>
2026-05-04 15:26:20 +0700

    939: Refactor visit_if_node method in ConditionalBranch class for imp… (#940)
    * 939: Refactor visit_if_node method in ConditionalBranch class for improved readability and reduced complexity
    
    * 939: Refactor visit_if_node method to improve handling of else branches and add tests for if/elsif/else scenarios

--- lib/evilution/mutator/operator/conditional_branch.rb
+++ lib/evilution/mutator/operator/conditional_branch.rb
@@ -4,29 +4,29 @@ require_relative "../operator"
 
 class Evilution::Mutator::Operator::ConditionalBranch < Evilution::Mutator::Base
   def visit_if_node(node)
-    if node.statements && node.subsequent.nil?
-      add_mutation(
-        offset: node.statements.location.start_offset,
-        length: node.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
-    elsif node.statements && node.subsequent&.statements
-      add_mutation(
-        offset: node.statements.location.start_offset,
-        length: node.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
+    return super unless node.statements
 
-      add_mutation(
-        offset: node.subsequent.statements.location.start_offset,
-        length: node.subsequent.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
-    end
+    add_nil_mutation(node.statements, node)
+    add_nil_mutation_to_else(node.subsequent, node)
 
     super
+  end
+
+  private
+
+  def add_nil_mutation_to_else(subsequent, node)
+    return unless subsequent.is_a?(Prism::ElseNode)
+    return if subsequent.statements.nil?
+
+    add_nil_mutation(subsequent.statements, node)
+  end
+
+  def add_nil_mutation(statements, node)
+    add_mutation(
+      offset: statements.location.start_offset,
+      length: statements.location.length,
+      replacement: "nil",
+      node: node
+    )
   end
 end
