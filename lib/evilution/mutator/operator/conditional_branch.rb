10d032d7bc233bbb98cc7baf5ce55739e199450e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-05-04 09:28:37 +0700

    939: Refactor visit_if_node method in ConditionalBranch class for improved readability and reduced complexity

--- lib/evilution/mutator/operator/conditional_branch.rb
+++ lib/evilution/mutator/operator/conditional_branch.rb
@@ -4,29 +4,23 @@ require_relative "../operator"
 
 class Evilution::Mutator::Operator::ConditionalBranch < Evilution::Mutator::Base
   def visit_if_node(node)
-    if node.statements && node.subsequent.nil?
-      add_mutation(
-        offset: node.statements.location.start_offset,
-        length: node.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
-    elsif node.statements && node.subsequent&.statements
-      add_mutation(
-        offset: node.statements.location.start_offset,
-        length: node.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
+    return super unless node.statements
+    return super if node.subsequent && node.subsequent.statements.nil?
 
-      add_mutation(
-        offset: node.subsequent.statements.location.start_offset,
-        length: node.subsequent.statements.location.length,
-        replacement: "nil",
-        node: node
-      )
-    end
+    add_nil_mutation(node.statements, node)
+    add_nil_mutation(node.subsequent.statements, node) if node.subsequent
 
     super
+  end
+
+  private
+
+  def add_nil_mutation(statements, node)
+    add_mutation(
+      offset: statements.location.start_offset,
+      length: statements.location.length,
+      replacement: "nil",
+      node: node
+    )
   end
 end
