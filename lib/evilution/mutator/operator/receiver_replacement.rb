122cc15f5b831738d74665350c229be911a82fe0
Denis Kiselev <denis.kiselyov@gmail.com>
2026-05-05 23:44:02 +0700

    1010: Refactor visit_call_node method: extract call_without_self logic into a private method for improved clarity (#1037)

--- lib/evilution/mutator/operator/receiver_replacement.rb
+++ lib/evilution/mutator/operator/receiver_replacement.rb
@@ -5,19 +5,22 @@ require_relative "../operator"
 class Evilution::Mutator::Operator::ReceiverReplacement < Evilution::Mutator::Base
   def visit_call_node(node)
     if node.receiver.is_a?(Prism::SelfNode)
-      call_without_self = @file_source.byteslice(
-        node.message_loc.start_offset,
-        node.location.start_offset + node.location.length - node.message_loc.start_offset
-      )
-
       add_mutation(
         offset: node.location.start_offset,
         length: node.location.length,
-        replacement: call_without_self,
+        replacement: call_without_self_text(node),
         node: node
       )
     end
 
     super
+  end
+
+  private
+
+  def call_without_self_text(node)
+    message_start = node.message_loc.start_offset
+    call_end = node.location.start_offset + node.location.length
+    @file_source.byteslice(message_start, call_end - message_start)
   end
 end
