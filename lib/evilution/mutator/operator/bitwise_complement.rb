6528ed348e17e2cb83c7f15f1411aba5fe7ab12e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-05-06 00:40:57 +0700

    998: Refactor visit_call_node method: extract complement removal and unary minus logic into separate methods for improved clarity and organization (#1075)

--- lib/evilution/mutator/operator/bitwise_complement.rb
+++ lib/evilution/mutator/operator/bitwise_complement.rb
@@ -5,27 +5,34 @@ require_relative "../operator"
 class Evilution::Mutator::Operator::BitwiseComplement < Evilution::Mutator::Base
   def visit_call_node(node)
     if node.name == :~ && node.receiver && node.arguments.nil?
-      loc = node.message_loc
-      receiver_loc = node.receiver.location
-
-      # Remove ~: replace entire ~expr with just the receiver expression
-      receiver_source = byteslice_source(receiver_loc.start_offset, receiver_loc.length)
-      add_mutation(
-        offset: node.location.start_offset,
-        length: node.location.length,
-        replacement: receiver_source,
-        node: node
-      )
-
-      # Swap ~ with unary minus
-      add_mutation(
-        offset: loc.start_offset,
-        length: loc.length,
-        replacement: "-",
-        node: node
-      )
+      emit_remove_complement(node)
+      emit_swap_to_minus(node)
     end
 
     super
+  end
+
+  private
+
+  # Replace `~expr` with just `expr`.
+  def emit_remove_complement(node)
+    receiver_loc = node.receiver.location
+    add_mutation(
+      offset: node.location.start_offset,
+      length: node.location.length,
+      replacement: byteslice_source(receiver_loc.start_offset, receiver_loc.length),
+      node: node
+    )
+  end
+
+  # Swap `~` with unary minus.
+  def emit_swap_to_minus(node)
+    loc = node.message_loc
+    add_mutation(
+      offset: loc.start_offset,
+      length: loc.length,
+      replacement: "-",
+      node: node
+    )
   end
 end
