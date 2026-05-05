51c1a9d5180e10972a2b61c4667da91e6bf68dd1
Denis Kiselev <denis.kiselyov@gmail.com>
2026-05-06 00:38:33 +0700

    997: Refactor visit_begin_node method: streamline unwrapping logic and improve clarity by extracting body_text and unwrappable? methods (#1074)

--- lib/evilution/mutator/operator/begin_unwrap.rb
+++ lib/evilution/mutator/operator/begin_unwrap.rb
@@ -4,18 +4,30 @@ require_relative "../operator"
 
 class Evilution::Mutator::Operator::BeginUnwrap < Evilution::Mutator::Base
   def visit_begin_node(node)
-    return super if node.rescue_clause || node.else_clause || node.ensure_clause
-    return super if node.statements.nil?
-    return super if node.begin_keyword_loc.nil?
+    return super unless unwrappable?(node)
 
-    body_text = @file_source.byteslice(node.statements.location.start_offset, node.statements.location.length)
     add_mutation(
       offset: node.location.start_offset,
       length: node.location.length,
-      replacement: body_text,
+      replacement: body_text(node),
       node: node
     )
 
     super
+  end
+
+  private
+
+  def unwrappable?(node)
+    return false if node.rescue_clause || node.else_clause || node.ensure_clause
+    return false if node.statements.nil?
+    return false if node.begin_keyword_loc.nil?
+
+    true
+  end
+
+  def body_text(node)
+    loc = node.statements.location
+    @file_source.byteslice(loc.start_offset, loc.length)
   end
 end
