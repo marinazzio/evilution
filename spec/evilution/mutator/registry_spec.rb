06b6005704591e9092a2d2454b8b9f0c5e41cc8e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-09-19 16:26:00 +0700

    Add OptionalParameterToRequired operator and update related documentation (#1613)

--- spec/evilution/mutator/registry_spec.rb
+++ spec/evilution/mutator/registry_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Evilution::Mutator::Registry do
       default_registry = described_class.default
 
       expect(default_registry).to be_a(described_class)
-      expect(default_registry.operator_count).to eq(84)
+      expect(default_registry.operator_count).to eq(85)
     end
 
     it "includes all expected operator classes" do
@@ -90,6 +90,7 @@ RSpec.describe Evilution::Mutator::Registry do
         Evilution::Mutator::Operator::MethodBodyToSuper,
         Evilution::Mutator::Operator::TypedDefaultReturn,
         Evilution::Mutator::Operator::BlockParameterDrop,
+        Evilution::Mutator::Operator::OptionalParameterToRequired,
         Evilution::Mutator::Operator::StringInterpolation,
         Evilution::Mutator::Operator::RetryRemoval,
         Evilution::Mutator::Operator::CaseWhen,
