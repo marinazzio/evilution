bc13a15a5b1ed636e9ab7e951d5487ea438f6cf2
Denis Kiselev <denis.kiselyov@gmail.com>
2026-09-25 17:21:23 +0700

    Add keyword_argument_removal operator and update documentation for new operator count

---
+++ spec/support/fixtures/keyword_argument_removal.rb
@@ -0,0 +1,44 @@
+class KeywordArgumentRemovalTarget
+  def two_keywords
+    build(name: "x", size: 2)
+  end
+
+  def positional_and_keyword(value)
+    build(value, name: "x")
+  end
+
+  def multiline
+    build(
+      name: "x",
+      size: 2
+    )
+  end
+
+  def with_double_splat(opts)
+    build(name: "x", **opts)
+  end
+
+  def string_keys
+    build("name" => "x", "size" => 2)
+  end
+
+  def shorthand(name, size)
+    build(name:, size:)
+  end
+
+  def with_block_pass(block)
+    build(name: "x", size: 2, &block)
+  end
+
+  def without_parens
+    build name: "x", size: 2
+  end
+
+  def lone_keyword
+    build(name: "x")
+  end
+
+  def hash_literal
+    build({ name: "x", size: 2 })
+  end
+end
